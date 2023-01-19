7e8e13c94a51e2e847da53bd2e78133e3b30c1e6
Elena Russkykh <lenarusskih@gmail.com>
2023-01-19 10:41:05 +0200

    firs commit

---
+++ Sources/CodingStyle/Extension+CodingStyle.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+CodingStyle.swift
+//  CodingStyle
+//
+//  Created by Елена Русских on 18.01.2023.
+//
+
+import Foundation
+
+extension CodingStyle{
+public  enum Style {
+        case camelCase, snakeCase, kebabCase
+    }
+}
